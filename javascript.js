25b491b008dc600ba9ec581b2b3cf9b1217b9f50
congnguyenchincc <61679730+congnguyenchincc@users.noreply.github.com>
2020-03-03 08:49:24 +0700

    fix

--- javascript.js
+++ javascript.js
@@ -1,15 +1,28 @@
 function move() {
-	var elem = document.getElementById('sibar');
-	var pos = 0;
-	var id = setInterval(frame, 5);
-	function frame() {
-		if (pos == 10) {
-			clearInterval(id);
-			} else {
-			pos++;
-			elem.style.left = pos + "px";
-			console.log(elem.style.left)
-			};
-
+	var stt;
+	var x=document.getElementById("sibar").querySelectorAll("img");
+	var y= document.getElementById("sibar").querySelectorAll("p");
+	var i,j;
+	if (document.getElementById('sibar').style.left!= 8+"px"){
+		stt=false;
 	}
+	if(stt==false){
+	document.getElementById('sibar').style.left= 8+"px";
+	for ( i=0; i< x.length; i++){
+		x[i].style.float = 'left';
+	}
+	for ( j = 0; j < y.length; j++) {
+			y[j].style.visibility='visible';
+	}
+	stt = true;
+	} else {
+		document.getElementById('sibar').style.left= -200+"px";
+		for ( i=0;i<x.length;i++){
+			for (j = 0; j < y.length;j++) {
+				y[j].style.visibility='hidden';
+				x[i].style.float = 'right';
+			}
+		}
+	}
+
 }
